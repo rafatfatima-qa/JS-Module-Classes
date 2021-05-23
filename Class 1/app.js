64897a545bd5f533854fb2538848dd56681f5b85
unknown <fatimaxaidi@yahoo.com>
2021-05-23 13:35:00 +0500

    Implementing if-else statements in javascript

--- Class 1/app.js
+++ Class 1/app.js
@@ -1,5 +1,25 @@
-const name = "Rafat Fatima";
-console.log(name);
-name = "ali";
-console.log(name);
+// const name = "Rafat Fatima";
+// console.log(name);
+// name = "ali";
+// console.log(name);
 //const name = "ali";
+
+function printvariable (name){
+
+let name1 = name;
+console.log(name1);
+if (name1 == "Fatima") {
+    
+    alert("Yay, Name is matching to Fatima!");
+  }
+
+else
+
+  alert("Oops! Name is NOT matching.");
+
+}
+
+
+// printvariable(Rafat);
+// printvariable(Fatima);
+// printvariable(Zaidi);
