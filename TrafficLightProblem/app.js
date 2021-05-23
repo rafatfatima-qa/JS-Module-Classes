96de64eb8849374190938d894eba079875cb134b
unknown <fatimaxaidi@yahoo.com>
2021-05-23 14:30:07 +0500

    Traffic Lights Problem

---
+++ TrafficLightProblem/app.js
@@ -0,0 +1,27 @@
+function printvariable (){
+
+let mycolor = prompt('Please input some color');
+console.log(mycolor);
+
+//conditions
+if (mycolor == "Red") {
+    
+    alert("This is Red Light!");
+  }
+
+ else if (mycolor == "Green") {
+    
+    alert("This is Green Light!");
+  }
+
+  else if (mycolor == "Yellow") {
+    
+    alert("This is Yellow Light!");
+  }
+
+  else (mycolor == "White" || mycolor == "Blue" || mycolor == " ")
+
+    alert("Oops this is not a traffic light color!");
+}
+
+
